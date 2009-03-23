9d29f8147235906ff115057076b0ab16d47fac47
Nikolaj Šujskij <skrattaren@yandex.ru>
2009-03-23 18:00:25 +0300

    [svn r27] Added usage of MPD_* variables and few comment lines

--- musik/psi-mpd_tune.sh
+++ musik/psi-mpd_tune.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+# A script to set your NowPlayingTune from MPD in Psi
+# See http://my.opera.com/Sterkrig/blog/psi-mpd for a little bit
+# more detailed description and/or suggestions etc.
+
+# Default MPD settings are (as for mpc) localhost:6600, without password
+# To set other values, use environment variables MPD_HOST, MPD_PORT, MPD_PASSWORD
+# Either export them, or run script that way:
+# $ MPD_HOST="nonlocalhost" MPD_PORT="6666" ./psi_tune.sh
+
 mpc --format "%title%\n%artist%\n%album%\n%track%\n%time%" | head -n -2 > ~/.psi/tune
 
 while [ 1 ]
