65bb041f3c533ede61dbe34d1c7d3788c3d70bdb
Nikolaj Šujskij <skrattaren@yandex.ru>
2009-11-03 20:22:37 +0300

    Updated comments for mpc 0.18

--- musik/psi-mpd_tune.sh
+++ musik/psi-mpd_tune.sh
@@ -10,10 +10,12 @@
 # To set other values, use environment variables MPD_HOST, MPD_PORT, MPD_PASSWORD
 # Either export them, or run script that way:
 # $ MPD_HOST="nonlocalhost" MPD_PORT="6666" ./psi_tune.sh
-
+#
 # This version uses 'mpc idle' command for convenience. It requires mpc 0.16 and mpd 0.14
 # If you use older versions, see older revisions
-# 'mpc idle' is known to consume CPU on some systems, so see above
+#
+# 'mpc idle' is known to consume CPU on some systems
+# (seems to be fixedfixed in 0.18), so see above
 
 mpc --format "%title%\n%artist%\n%album%\n%track%\n%time%" | head -n -2 > ~/.psi/tune
 
