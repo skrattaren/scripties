db776d42d6ddc9bc65d7ab19261f9236fe6193d5
Nikolaj Šujskij <skrattaren@yandex.ru>
2009-12-06 13:45:03 +0300

    Script directory and screenshooting script

---
+++ shoot.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+SWITCH=''
+PATH="${HOME}/bilder/tmp"
+
+if [[ $1 == '-s' ]]
+    then SWITCH='-s'
+         FILENAME=$2
+    else FILENAME=$1
+fi
+
+FILE=$PATH/$FILENAME
+
+/usr/bin/scrot $SWITCH $FILE
+/usr/bin/optipng $FILE
+/usr/bin/uimge -i --usr=#url# $FILE
+
