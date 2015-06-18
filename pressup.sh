f0318d8b49b0b3a1f5910ed4f40ad136447f5238
Nikolaj Šujskij <skrattaren@yandex.ru>
2015-06-18 22:40:39 +0300

    pressup: use standard path to alarm file

--- pressup.sh
+++ pressup.sh
@@ -34,7 +34,7 @@ for i in {2..$ITERATIONS}; do
     read -p "Press Enter when finished..."
     echo "Waiting $PRESSUP_INT seconds..."
     countdown $PRESSUP_INT
-    mpv --msglevel=all=fatal /Applications/Psi.app/Contents/Resources/sound/ft_complete.wav
+    mpv --msglevel=all=fatal ~/.pressup.snd
     echo "Set #${i}"
 done
 echo "No need to press everything when finished, just do it (-:E"
