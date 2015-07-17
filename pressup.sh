cf3f2394ab3af4b91fcb4f03346cf6a3f5555bc8
Nikolaj Šujskij <skrattaren@yandex.ru>
2015-07-17 19:11:09 +0300

    pressup: add "docs" and license info

--- pressup.sh
+++ pressup.sh
@@ -1,5 +1,47 @@
 #!/bin/bash
 
+# This script is intended to be a helper for those who follow famous
+# "One hundred pushups" fitness program: http://www.hundredpushups.com/
+# (or similar ones).
+
+### What does it do?
+# Basically script just notifies you when it's time to start a new set of
+# exercises with sound and terminal bell.  Default settings are 5 sets with
+# number of seconds to rest in between determined automagically depending on
+# day of the week (Mon and Tue -- 60 sec, Wed and Thu -- 90 sec, Fri and Sat --
+# 120 sec), which matches the first 4 weeks of OHP program.  Of course, it's
+# possible to override those, and script is a bit heuristic: if you pass it an
+# argument, it assumes that number less than 21 must be number of sets, and
+# number greater is probably a rest interval. So (examples run on Friday):
+
+#  % ./pressup.sh
+# Counting for 5 sets by 120 seconde
+#
+#  % ./pressup.sh 8
+# Counting for 8 sets by 120 seconde
+#
+#  % ./pressup.sh 8 45
+# Counting for 8 sets by 45 seconde
+#
+#  % ./pressup.sh 45 8
+# Counting for 8 sets by 45 seconde
+#
+#  % ./pressup.sh 45
+# Counting for 5 sets by 45 seconde
+
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the `GNU General Public License`_ for details.
+#
+# .. GNU General Public License: http://www.gnu.org/licenses/
+# (c) 2014-2015, Николай Шуйский (Nikolaj Sjujskij) <skrattaren@yandex.ru>
+
+
 PRESSUP_INT=
 ITERATIONS=
 
